14753afab26fa629f9660101104087c61456c807
FhLk <68800909+FhLk@users.noreply.github.com>
2024-05-26 01:03:46 +0700

    Setup Project + Create Page

---
+++ school-management-system/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Teacher Page</h1>
+      <p>Mr. Paramat Pet-in</p>
+      <p>Subject: Computor Programing</p>
+    </div>
+  )
+}
+
+export default App
